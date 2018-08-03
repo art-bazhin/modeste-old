1166c1dc9c0ae8efca40c04a81e1e60ef655eab7
Артем Бажин <bazhin@selectel.ru>
2018-08-03 19:37:02 +0300

    Updated build config

--- rollup.config.js
+++ rollup.config.js
@@ -5,39 +5,33 @@ import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		entry: 'src/main.js',
-		dest: pkg.browser,
-		format: 'umd',
-		moduleName: 'Just',
-		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs(), // so Rollup can convert `ms` to an ES module
-			babel({
-				exclude: ['node_modules/**']
-			}),
-      		uglify()
-		]
-	}, 
+  // browser-friendly iife build
+  {
+    entry: 'src/main.js',
+    dest: pkg.main,
+    format: 'umd',
+    moduleName: 'Just',
+    sourceMap: true,
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      babel({
+        exclude: ['node_modules/**']
+      }),
+      uglify()
+    ]
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		entry: 'src/main.js',
-		external: ['ms'],
-		targets: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
-		],
-		plugins: [
-			// babel({
-			// 	exclude: ['node_modules/**']
-			// })
-		]
-	}
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    entry: 'src/main.js',
+    external: ['ms'],
+    format: 'es',
+    dest: pkg.module
+  }
 ];
