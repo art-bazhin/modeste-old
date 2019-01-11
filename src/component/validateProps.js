5ce40e094e72c1eb966aba08b2c52b5ee01e90fc
Артем Бажин <bazhin@selectel.ru>
2019-01-11 20:24:32 +0300

    Update props validation

--- src/component/validateProps.js
+++ src/component/validateProps.js
@@ -1,7 +1,59 @@
 import { INTERNAL_VAR_NAME as m } from '../constants';
 import ModesteError from '../utils/ModesteError';
 
+function validatePropType(prop, type, key, component, isFirstCheck) {
+  if (prop === null || type === 'any') return true;
+
+  switch (typeof type) {
+    case 'string':
+      if (prop === undefined) return true;
+      if (typeof prop !== type) {
+        throw new ModesteError(
+          `${component[m].name}: ${key} prop must be a type ${type}`
+        );
+      }
+      break;
+    case 'function':
+      if (prop === undefined) return true;
+      if (!(prop instanceof type)) {
+        throw new ModesteError(
+          `${component[m].name}: ${key} prop must be an instance of ${
+            type.name
+          }`
+        );
+      }
+      break;
+    default:
+      if (isFirstCheck) return validatePropObject(prop, type, key, component);
+      throw new ModesteError(
+        `${component[m].name}: wrong ${key} prop type description`
+      );
+  }
+
+  return true;
+}
+
+function validatePropObject(prop, obj, key, component) {
+  validatePropType(prop, obj.type, key, component);
+
+  if (obj.required && prop === undefined) {
+    throw new ModesteError(
+      `${component[m].name}: ${key} is required property of the component`
+    );
+  }
+
+  if (obj.validator && !obj.validator(prop)) {
+    throw new ModesteError(
+      `${component[m].name}: ${key} prop didn't pass custom validation`
+    );
+  }
+
+  return true;
+}
+
 export default function validateProps(props, validationObject, component) {
+  if (!Object.keys(props).length && !validationObject) return true;
+
   let validationKeys = Object.keys(validationObject);
   let propKeys = Object.keys(props);
 
@@ -14,11 +66,7 @@ export default function validateProps(props, validationObject, component) {
   });
 
   validationKeys.forEach(key => {
-    if (validationObject[key] && propKeys.indexOf(key) < 0) {
-      throw new ModesteError(
-        `${component[m].name}: ${key} is required property of the component`
-      );
-    }
+    validatePropType(props[key], validationObject[key], key, component, true);
   });
 
   return true;
